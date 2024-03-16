ab3dd676ae31373bf9b708c3be07b7e3199de714
Nikos Skliris <nikoskliris@gmail.com>
2024-03-16 18:23:48 +0200

    Clerk Authentication added - CSS fixed for login page

---
+++ tasktracker/app/(platform)/protected/page.tsx
@@ -0,0 +1,8 @@
+"use client;";
+
+import { UserButton } from "@clerk/nextjs";
+
+const ProtectedPage = async () => {
+  return <UserButton afterSignOutUrl="/" />;
+};
+export default ProtectedPage;
---
+++ tasktracker/config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: "Task Progress Tracker",
+  description: "A simple way to track your team's tasks and progress.",
+};
